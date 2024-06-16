dd8317c9d4e1c99078de530cb35f03e37b825bf3
Ajitkumar Sharma <77828414+Ajit619@users.noreply.github.com>
2024-06-16 14:15:23 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+//     auto a = 5;
+//     cout<<a;
+//     return 0;
+    
+
+vector<int> v1;
+vector<char> v2(4);
+vector<int> v3(5, 10);
+}
